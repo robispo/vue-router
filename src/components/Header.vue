6a0b992cfc8cacd6048460427200f49b411c9337
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-25 22:50:13 -0500

    Nav

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <ul class="nav nav-tabs">
+      <router-link to="/" tag="li" active-class="active" exact>
+        <a>Home</a>
+      </router-link>
+      <router-link to="/user" tag="li" active-class="active">
+        <a>User</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped></style>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import Home from './components/Home.vue';
 import User from './components/user/User.vue';
 
 export const routes = [
-         { path: '', component: Home },
-         { path: '/user', component: User }
-       ];
+  { path: '/user', component: User },
+  { path: '', component: Home }
+];
