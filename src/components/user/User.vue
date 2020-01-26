4b09654c9fd075103d05b21ea9d34cc16f8ab3e4
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-25 22:57:58 -0500

    Going back

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,19 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr />
+    <button class="btn btn-primary" @click.prevent="navHome">Home</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      navHome() {
+        this.$router.push({ path: '/' });
+      }
+    }
+  };
+</script>
+
+<style scoped></style>
