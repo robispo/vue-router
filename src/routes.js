d747535105b880a164da8f2dda7accd77996762b
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-29 20:53:54 -0500

    multiple router views

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,28 @@ import User from './components/user/User.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
+import Header from './components/Header.vue';
 
 export const routes = [
-  { path: '', component: Home, name:'home' },
-  {
-    path: '/user',
-    component: User,
-    children: [
-      { path: '', component: UserStart },
-      { path: ':id', component: UserDetail },
-      { path: ':id/edit', component: UserEdit, name: 'userEdit' }
-    ]
-  }
-];
+         {
+           path: '',
+           components: {
+             default: Home,
+             'header-top': Header,
+             'header-bottom': Header
+           },
+           name: 'home'
+         },
+         {
+           path: '/user',
+           components: {
+             default: User,
+             'header-top': Header
+           },
+           children: [
+             { path: '', component: UserStart },
+             { path: ':id', component: UserDetail },
+             { path: ':id/edit', component: UserEdit, name: 'userEdit' }
+           ]
+         }
+       ];
