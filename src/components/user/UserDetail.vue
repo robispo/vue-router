412445ae5b04eefff50c5ba11df9b026865e5a03
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-29 21:27:26 -0500

    Pasing hash in routes

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -3,16 +3,23 @@
     <h3>Some User Details</h3>
     <p>User loaded with ID: {{ $route.params.id }}</p>
     <br />
-    <router-link
-      tag="button"
-      :to="{
-        name: 'userEdit',
-        parmas: { id: $route.params.id },
-        query: { locale: 'en', q: 100 }
-      }"
-      class="btn btn-primary"
-    >
+    <router-link tag="button" :to="link" class="btn btn-primary">
       Edit
     </router-link>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        link: {
+          name: 'userEdit',
+          parmas: { id: this.$route.params.id },
+          query: { locale: 'en', q: 100 },
+          hash: '#data'
+        }
+      };
+    }
+  };
+</script>
