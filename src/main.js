c8321a9ecdb952ba5cf092ac3fc8c31a16074e0b
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-29 21:36:29 -0500

    Controlling scroll with routes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 
 Vue.use(VueRouter);
-const router = new VueRouter({ routes, mode: 'history' });
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  }
+});
 
 new Vue({
   el: '#app',
